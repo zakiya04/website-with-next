314d16c6b0d80bbfd466d5af3182b6a15958f461
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-03 09:13:06 +0530

    added to product folder

---
+++ src/components/product/mainapi.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+async function API() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const products = await res.json();
+  return products;
+}
+
+export default API;
